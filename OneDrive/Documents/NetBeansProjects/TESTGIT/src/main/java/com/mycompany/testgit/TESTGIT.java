73f1b5960aad760750a993114377d14e5ec306ed
Yahiakratoss <122739743+Yahiakratoss@users.noreply.github.com>
2024-11-18 18:56:54 +0100

    first test

---
+++ OneDrive/Documents/NetBeansProjects/TESTGIT/src/main/java/com/mycompany/testgit/TESTGIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgit;
+
+/**
+ *
+ * @author Yahia
+ */
+public class TESTGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
